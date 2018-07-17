11b5b7d9d096a129752ee2e818d8f04af97bfdc8
Simon Gordon <gordonsimon@hotmail.com>
2018-07-17 21:09:33 +1000

    Add string function

--- src/index.js
+++ src/index.js
@@ -6,5 +6,8 @@ wasm.initialize({ noExitRuntime: true }).then(module => {
   const add = module.cwrap('add', 'number', ['number', 'number'])
   console.log('Calling rust functions from javascript!')
   console.log(add(1, 2))
+
+  const getData = module.cwrap('get_data', 'string');
+  console.log(getData());
 })
 
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::os::raw::c_char;
+use std::collections::HashMap;
+use std::ffi::CString;
+
+#[no_mangle]
+pub fn get_data() -> *mut c_char {
+    let mut data = HashMap::new();
+    data.insert("Alice", "send");
+    data.insert("Bob", "recieve");
+    data.insert("Carol", "intercept");
+
+    let descriptions = data.iter()
+        .map(|(p,a)| format!("{} likes to {} messages", p, a))
+        .collect::<Vec<_>>();
+
+    CString::new(descriptions.join(", "))
+        .unwrap()
+        .into_raw()
+}
+
 #[no_mangle]
 pub fn add(a: i32, b: i32) -> i32 {
     a + b
